64387497dbb3fbdeee6870f4dc0429d00342cf32
camachoo18 <estivencamachops@gmail.com>
2023-11-30 13:02:44 +0100

    Probando picocss

---
+++ script.js
@@ -0,0 +1,16 @@
+//const data= document.querySelector("#range").value
+//console.log(data)
+//document.querySelector("#progressbar").value = data
+
+//
+
+let contador = 0
+function suma(){
+contador++
+document.querySelector(".contador").innerHTML = contador
+}
+let contador1=0
+function restar(){
+    contador--
+    document.querySelector(".contador").innerHTML=contador
+}
